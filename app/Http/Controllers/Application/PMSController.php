0195c92b109a31bf95d6104c5c7a360ba710b7dd
kingaseldon <kingaseldon36@gmail.com>
2026-04-21 12:07:08 +0600

    c

--- app/Http/Controllers/Application/PMSController.php
+++ app/Http/Controllers/Application/PMSController.php
@@ -1541,7 +1541,7 @@ Z1.AppraisedByEmployeeId = ?) on T2.EmployeeId = T1.Id and (DATE_FORMAT(T2.Submi
                  with revised Basic salary of <strong><em>Nu. $newBasicPay </em></strong> per month with the pay scale of <strong><em>($newPayScale)</em></strong>, with effect from <strong><em>$effectiveDate</em></strong> <br/><br/> ";
 
                 $content .= "Your commitment, efficiency, and professional input have significantly contributed to the overall success of <strong>$employeeDept</strong>. We sincerely appreciate your effort and positive impact of your work. We trust that you will maintain, and indeed surpass, this level of performance in the future. <br/><br/>
-                All other service terms shall be as per the appointment order and service rule of the company in force. </br></br>
+                All other service terms shall be as per the contract of employment and service rule of the company in force. </br></br>
 
                 ";
                 // if (!is_null($HraAllowance)) {
